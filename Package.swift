2c15e19854aa02fe1a01146e3fcd91d289482fee
Amr Aboelela <amraboelela@gmail.com>
2019-02-18 17:53:51 -0800

    Package.swift

--- Package.swift
+++ Package.swift
@@ -19,11 +19,5 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "CommonCrypto",
-            dependencies: []),
-        .testTarget(
-            name: "CommonCryptoTests",
-            dependencies: ["CommonCrypto"]),
     ]
 )
