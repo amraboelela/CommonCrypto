504a8eeebe614c28f32447fa4d88a451dfac7d1c
Amr Aboelela <amraboelela@gmail.com>
2022-08-26 18:12:29 -0700

    swift-tools-version:5.5

--- Package.swift
+++ Package.swift
@@ -1,3 +1,4 @@
+
 // swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
@@ -7,11 +8,14 @@ let package = Package(
     name: "CommonCrypto",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(name: "CommonCrypto", targets: ["CommonCrypto"]),
+        .library(
+            name: "CommonCrypto",
+            targets: ["CommonCrypto"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        //.package(url: "https://github.com/amraboelela/CLevelDB", .branch("master"))
     ],
     targets: [
         .systemLibrary(name: "CommonCrypto", path: "."),
